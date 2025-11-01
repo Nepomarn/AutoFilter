2bbc4745bc5d39a790a96054bcfd2e9d97c84b89
Nepomarn <137699149+Nepomarn@users.noreply.github.com>
2025-11-01 19:38:47 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,13 @@
 import sys
+from pyromod import listen
 import glob
 import importlib
 from pathlib import Path
-from pyrogram import Client, idle, __version__
-from pyrogram.raw.all import layer
+from pyrogram import idle
 import logging
 import logging.config
-import time
-import asyncio
-from datetime import date, datetime
-import pytz
-from aiohttp import web
 
-from database.ia_filterdb import Media, Media2, choose_mediaDB, tempDict, db as clientDB
-from database.users_chats_db import db
-from info import *
-from utils import temp
-from Script import script
-from plugins import web_server, check_expired_premium
-from LucyBot.Bot import Codeflix
-from LucyBot.util.keepalive import ping_server
-from LucyBot.Bot.clients import initialize_clients
-
+# Logging setup
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -33,16 +19,34 @@ logging.basicConfig(
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-botStartTime = time.time()
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from info import *
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from Script import script
+from datetime import date, datetime
+import pytz
+from aiohttp import web
+from plugins import web_server
+
+import asyncio
+from pyrogram import idle
+from lazybot import LazyPrincessBot
+from util.keepalive import ping_server
+from lazybot.clients import initialize_clients
+
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
+LazyPrincessBot.start()
+loop = asyncio.get_event_loop()
 
-async def Lucy_start():
+async def Lazy_start():
     print('\n')
-    print('\nInitalizing Lucy')
-    await Codeflix.start()
-    bot_info = await Codeflix.get_me()
-    Codeflix.username = bot_info.username
+    print('Initalizing Lucy Bot')
+    bot_info = await LazyPrincessBot.get_me()
+    LazyPrincessBot.username = bot_info.username
     await initialize_clients()
     for name in files:
         with open(name) as a:
@@ -54,49 +58,35 @@ async def Lucy_start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Lucy Bot Imported => " + plugin_name)
+            print("Lucy Imported => " + plugin_name)
     if ON_HEROKU:
-        asyncio.create_task(ping_server()) 
-    b_users, b_chats = await db.get_banned()
-    temp.BANNED_USERS = b_users
-    temp.BANNED_CHATS = b_chats
-    await Media.ensure_indexes()
-    await Media2.ensure_indexes()
-    stats = await clientDB.command('dbStats')
-    free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
-    if DATABASE_URI2 and free_dbSize<62: #if the primary db have less than 62MB left, use second DB.
-        tempDict["indexDB"] = DATABASE_URI2
-        logging.info(f"Since Primary DB have only {free_dbSize} MB left, Secondary DB will be used to store datas.")
-    elif DATABASE_URI2 is None:
-        logging.error("Missing second DB URI !\n\nAdd SECONDDB_URI now !\n\nExiting...")
-        exit()
-    else:
-        logging.info(f"Since primary DB have enough space ({free_dbSize}MB) left, It will be used for storing datas.")
-    await choose_mediaDB()    
-    me = await Codeflix.get_me()
+        asyncio.create_task(ping_server())
+    # REMOVED: MongoDB-dependent banned users/chats and indexes
+    # b_users, b_chats = await db.get_banned()
+    # temp.BANNED_USERS = b_users
+    # temp.BANNED_CHATS = b_chats
+    # await Media.ensure_indexes()
+    me = await LazyPrincessBot.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    temp.B_LINK = me.mention
-    Codeflix.username = '@' + me.username
-    Codeflix.loop.create_task(check_expired_premium(Codeflix))
-    logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+    LazyPrincessBot.username = '@' + me.username
+    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await Codeflix.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
+    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
-    
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(Lucy_start())
+        loop.run_until_complete(Lazy_start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
